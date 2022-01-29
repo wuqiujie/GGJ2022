23930cb61bcfbaecf3d5073f7c81b86687a1ceac
wuqiujie <qiujiewu1@gmail.com>
2022-01-29 16:18:44 -0500

    SquidTouchBotton

--- Assets/Scripts/Background_Change.cs
+++ Assets/Scripts/Background_Change.cs
@@ -23,4 +23,5 @@ public class Background_Change : MonoBehaviour
             transform.position = Startpostion;
         }
     }
+    
 }
---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+   
+}
